133e7079172d02649ed7ee8d0767d775729cd3cc
michaelc445 <michaelc2468@gmail.com>
2023-02-16 19:50:46 +0000

    changed map to use mountain

--- main.py
+++ main.py
@@ -199,17 +199,34 @@ def run_once(loop):
 def multi_player_game_loop(game_client):
     #gameKeys = [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_r, pygame.K_t]
     pygame.init()
-    f1,f2 = None,None
+
+    p1_spawn = [100, 100]
+    p2_spawn = [850, 100]
+    map_chosen = "game/assets/maps/mountain.png"
+    # mountain obstacles
+    middle_ground1 = Obstacle(270, 410, 465, 60)
+    middle_ground2 = Obstacle(290, 470, 425, 60)
+    left_cliff1 = Obstacle(0, 235, 240, 60)
+    left_cliff2 = Obstacle(0, 295, 205, 60)
+    left_cliff3 = Obstacle(0, 355, 150, 60)
+    left_cliff4 = Obstacle(0, 405, 125, 300)
+    right_cliff1 = Obstacle(760, 235, 240, 60)
+    right_cliff2 = Obstacle(800, 295, 205, 60)
+    right_cliff3 = Obstacle(850, 355, 150, 60)
+    right_cliff4 = Obstacle(875, 405, 125, 300)
+
+    obstacles = [middle_ground1, middle_ground2, left_cliff1, left_cliff2, left_cliff3, left_cliff4, right_cliff1,
+                 right_cliff2, right_cliff3, right_cliff4]
 
     pygame.display.set_caption("Team 5 Project")
     char_dict = {0: createNomad, 1: createWizard, 2: createWarrior}
     if game_client.player_id == 0:
     # create fighters
-        f1 = char_dict[game_client.local_char](OnlineFighter, 1, 200, 310, False, punch_fx, projectile_fx, hit_fx,player1_controls)
-        f2 = char_dict[game_client.enemy_char](OnlineFighter, 2, 700, 310, True, punch_fx, projectile_fx, hit_fx,player1_controls)
+        f1 = char_dict[game_client.local_char](OnlineFighter, 1, p1_spawn[0], p1_spawn[1], False, punch_fx, projectile_fx, hit_fx,player1_controls)
+        f2 = char_dict[game_client.enemy_char](OnlineFighter, 2, p2_spawn[0], p2_spawn[1], True, punch_fx, projectile_fx, hit_fx,player1_controls)
     else:
-        f1 = char_dict[game_client.enemy_char](OnlineFighter, 1, 200, 310, False, punch_fx, projectile_fx, hit_fx, player1_controls)
-        f2 = char_dict[game_client.local_char](OnlineFighter, 2, 700, 310, True, punch_fx, projectile_fx, hit_fx, player1_controls)
+        f1 = char_dict[game_client.enemy_char](OnlineFighter, 1, p1_spawn[0], p1_spawn[1], False, punch_fx, projectile_fx, hit_fx, player1_controls)
+        f2 = char_dict[game_client.local_char](OnlineFighter, 2, p2_spawn[0], p2_spawn[1], True, punch_fx, projectile_fx, hit_fx, player1_controls)
 
     fighters = [f1, f2]
     pick = int(game_client.player_id)
@@ -218,9 +235,10 @@ def multi_player_game_loop(game_client):
     local_player.game_client = game_client
     enemy = (pick + 1) % 2
     enemy_character = fighters[enemy]
-    obstacle_1 = Obstacle(400, 300, 100, 300)
-    obstacle_2 = Obstacle(700, 200, 200, 50)
-    obstacles = [obstacle_1, obstacle_2]
+
+
+
+    bg_image = pygame.image.load(map_chosen).convert_alpha()
     run = True
     clock = pygame.time.Clock()
     FPS = 60
